cc211a262add9afaa8f09285fa2c0939038937a7
Ankur Yadav <ankur1236876@gmail.com>
2026-02-03 00:11:00 +0530

    Add files via upload

---
+++ Node.js-by-Code-with-Harry/Node.js In One Video/Second.js
@@ -0,0 +1,13 @@
+// (function(exports,require,module,__filename, __dirname){
+
+
+
+ankur = {
+    name: "Ankur",
+    favNum:3,
+    developer:true
+}
+
+console.log(exports,require,module,__filename, __dirname)
+  module.exports = ankur ;
+// })
